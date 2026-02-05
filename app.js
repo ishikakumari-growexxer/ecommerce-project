95a461b07ffd02f9b3173637c5a6a1140c3da4d8
Ishika Kumari <ishika.kumari@growexx.com>
2026-02-05 16:30:12 +0530

    Add products API endpoint

--- app.js
+++ app.js
@@ -11,3 +11,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
 console.log(`Server running on port ${PORT}`);
 });
+app.get('/api/products', (req, res) => {
+res.json({ products: [] });
+});
